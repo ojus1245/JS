14bcc53033403c4363593c81fda87569d91f37c7
ojus1245 <154688664+ojus1245@users.noreply.github.com>
2025-02-06 17:59:53 +0000

    changes done

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const account_id= 112432;
+let account_Name="Jamlal";
+var account_Password="Kallu_Kaliya";
+account_State="Jamshedpur";
+
+console.table([account_id,account_Name,account_Password,account_State]);
+
+//console.table is a better way to view the contents in a tabular and organised way
+//let and var are used to declare variables
+//var is avoided in modern js because of issues in scoping 
+
