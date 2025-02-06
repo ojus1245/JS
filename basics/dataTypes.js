4d3efe8cf83b0940e2233c70259f29ec38138772
ojus1245 <154688664+ojus1245@users.noreply.github.com>
2025-02-06 18:22:52 +0000

    changes done

---
+++ basics/dataTypes.js
@@ -0,0 +1,17 @@
+let a= 23;  //number range-2 pow 53  ,bigint
+let b="Rakesh";// string
+let c=true;// boolean
+let d=null;//null
+let e;//undefined
+let f={title:"bhoja",hero:"khushan",age:53,id:5} //object
+
+
+
+console.table([typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(f)]);
+
+/*Note:
+typeof(null) =>object
+typeof(undefined) =>undefined
+typeof(object) => object
+
+*/
